0775a93ec41a9bb49db08f763bac55d1fabb952a
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-06-06 23:54:31 -0300

    better product card

--- resources/views/web/takeaway.blade.php
+++ resources/views/web/takeaway.blade.php
@@ -19,7 +19,7 @@
         <div class="row grid">
           @foreach($aProductos as $producto)
             <div class="col-sm-6 col-lg-4 all categoria-{{$producto->fk_idcategoria}}">
-              <div class="box">
+              <article class="box product">
                 <div>
                   <div class="img-box">
                     <img src="files/{{$producto->imagen}}" alt="Imagen">
@@ -29,15 +29,19 @@
                     @if($producto->descripcion)
                     <p>{{$producto->descripcion}}</p>
                     @endif
-                    <div class="options">
+                    <div class="d-flex justify-content-between mt-auto">
                       <h6>${{ number_format($producto->precio, 2, ",", ".") }}</h6>
-                      <a href="">
-                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                      </a>
+                      <!-- TODO: Solo mostrar el botón si se ha iniciado sesión -->
+                      <form action="" method="POST" class="add-to-cart">
+                        <input type="number" name="txtCantidad" id="txtCantidad" value="1">
+                        <a href="">
+                          Añadir al carrito <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        </a>
+                      </form>
                     </div>
                   </div>
                 </div>
-              </div>
+              </article>
             </div>
           @endforeach
         </div>
