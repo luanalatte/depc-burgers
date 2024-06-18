8321b42ee63d7e0f1a7041101d403c65ad74c69c
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-06-17 21:12:06 -0300

    form nosotros

--- resources/views/web/nosotros.blade.php
+++ resources/views/web/nosotros.blade.php
@@ -5,22 +5,24 @@
   <section class="about_section layout_padding">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-5  ">
           <div class="img-box">
             <img src="web/images/about-img.png" alt="">
           </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 offset-md-1">
           <div class="detail-box">
-            <hgroup class="mb-2">
-              <h2>Nosotros</h2>
+            <hgroup class="mb-4">
+              <h2>Trabajá con nosotros</h2>
             </hgroup>
-            <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-              in some form, by injected humour, or randomised words which don't look even slightly believable. If you
-              are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in
-              the middle of text. All
-            </p>
+            <form action="" method="post">
+              <input class="form-control" type="text" name="txtNombre" id="txtNombre" placeholder="Nombre Completo">
+              <input class="form-control" type="email" name="txtEmail" id="txtEmail" placeholder="Correo Electrónico">
+              <input class="form-control" type="text" name="txtDomicilio" id="txtDomicilio" placeholder="Domicilio">
+              <input class="form-control" type="tel" name="txtTelefono" id="txtTelefono" placeholder="Celular / Whatsapp">
+              <label for="fileCV">Adjuntar Currículum:</label>
+              <input class="form-control-file" type="file" name="fileCV" id="fileCV" accept=".pdf, .doc, .docx">
+            </form>
           </div>
         </div>
       </div>
