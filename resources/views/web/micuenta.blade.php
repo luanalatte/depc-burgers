876680c7ba5a9a683f907cac3ab016751a5cb234
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-07-01 08:46:10 -0300

    micuenta improve

--- resources/views/web/micuenta.blade.php
+++ resources/views/web/micuenta.blade.php
@@ -2,64 +2,75 @@
 @section('contenido')
 <section class="layout_padding" id="micuenta">
   <div class="container">
-    <div class="row">
-      <div class="mb-5 col-12">
-        <hgroup class="mb-5">
-          <h2>Pedidos Activos</h2>
-        </hgroup>
-        <table class="table border">
-          <thead>
-            <tr>
-              <th>Pedido</th>
-              <th>Total</th>
-              <th>Estado</th>
-              <th>Pagado</th>
-              <th>Sucursal</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($aPedidos as $pedido)
-            <tr>
-              <td>{{ $pedido->idpedido }}</td>
-              <td>{{ $pedido->total }}</td>
-              <td>{{ $pedido->estado }}</td>
-              <td>{{ $pedido->pagado ? "Sí" : "No" }}</td>
-              <td>{{ $pedido->sucursal }}</td>
-              @if(!$pedido->pagado)
-              <td>
-                <a href="/mercadopago/pagar/{{ $pedido->idpedido }}" class="btn btn-primary">Pagar con Mercado Pago</a>
-              </td>
-              @else
-              <td></td>
-              @endif
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-      <div class="col-12">
-        <hgroup class="mb-5">
-          <h2>Mi Cuenta</h2>
-        </hgroup>
-        @include('web.msg')
-        <form action="" method="POST">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
+    @if(count($aPedidos) > 0)
+      <hgroup class="mb-5">
+        <h2>Pedidos Activos</h2>
+      </hgroup>
+      <table class="table border mb-5">
+        <thead>
+          <tr>
+            <th>Pedido</th>
+            <th>Total</th>
+            <th>Estado</th>
+            <th>Pagado</th>
+            <th>Sucursal</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($aPedidos as $pedido)
+          <tr>
+            <td>{{ $pedido->idpedido }}</td>
+            <td>{{ $pedido->total }}</td>
+            <td>{{ $pedido->estado }}</td>
+            <td>{{ $pedido->pagado ? "Sí" : "No" }}</td>
+            <td>{{ $pedido->sucursal }}</td>
+            @if(!$pedido->pagado)
+            <td>
+              <a href="/mercadopago/pagar/{{ $pedido->idpedido }}" class="btn btn-primary">Pagar con Mercado Pago</a>
+            </td>
+            @else
+            <td></td>
+            @endif
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    @endif
+    <hgroup class="mb-5">
+      <h2>Mi Cuenta</h2>
+    </hgroup>
+    @include('web.msg')
+    <form action="" method="POST" class="mb-3">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
+      <div class="row">
+        <div class="col-md 6">
           <label for="txtNombre">Nombre:</label>
           <input type="text" readonly class="form-control" value="{{ $cliente->nombre }}"></input>
+        </div>
+        <div class="col-md-6">
           <label for="txtApellido">Apellido:</label>
           <input type="text" readonly class="form-control" value="{{ $cliente->apellido }}"></input>
+        </div>
+        <div class="col-md-6">
           <label for="txtDocumento">Documento:</label>
           <input type="text" readonly class="form-control" value="{{ $cliente->dni }}"></input>
+        </div>
+        <div class="col-md-6">
           <label for="txtEmail">Correo Electrónico:</label>
           <input type="text" readonly class="form-control" value="{{ $cliente->email }}"></input>
+        </div>
+        <div class="col-md-6">
           <label for="txtTelefono">Celular / Whatsapp:</label>
           <input type="text" readonly class="form-control" value="{{ $cliente->telefono }}"></input>
+        </div>
+        <div class="col-12">
           <button type="submit" class="btn btn-primary mr-2">Guardar</button>
           <a href="/logout" class="btn btn-secondary">Cerrar Sesión</a>
-        </form>
+        </div>
       </div>
-    </div>
+    </form>
+    <a href="/cambiar-clave">Cambiar contraseña</a>
   </div>
 </section>
 @endsection
