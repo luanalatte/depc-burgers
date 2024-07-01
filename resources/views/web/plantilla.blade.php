29ce57d198852d59b1f441d0e031d2a1da8be63d
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-07-01 06:39:44 -0300

    fix paths

--- resources/views/web/plantilla.blade.php
+++ resources/views/web/plantilla.blade.php
@@ -11,26 +11,26 @@
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link rel="shortcut icon" href="web/images/favicon.png" type="">
+    <link rel="shortcut icon" href="/web/images/favicon.png" type="">
 
     <title>Burgers SRL</title>
 
-    <link href="web/css/style.min.css" rel="stylesheet" />
+    <link href="/web/css/style.min.css" rel="stylesheet" />
 
     <!--owl slider stylesheet -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <!-- nice select  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ==" crossorigin="anonymous" />
     <!-- font awesome style -->
-    <link href="web/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="/web/css/font-awesome.min.css" rel="stylesheet" />
 
     <!-- responsive style -->
-    <link href="web/css/responsive.css" rel="stylesheet" />
+    <link href="/web/css/responsive.css" rel="stylesheet" />
 </head>
 <body>
     <div class="hero_area">
         <div class="bg-box">
-            <img src="web/images/hero-bg.jpg" alt="">
+            <img src="/web/images/hero-bg.jpg" alt="">
         </div>
 
         <header>
@@ -159,12 +159,12 @@
     </footer>
     <!-- footer section -->
     <!-- jQuery -->
-    <script src="web/js/jquery-3.4.1.min.js"></script>
+    <script src="/web/js/jquery-3.4.1.min.js"></script>
     <!-- popper js -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <!-- bootstrap js -->
-    <script src="web/js/bootstrap.js"></script>
+    <script src="/web/js/bootstrap.js"></script>
     <!-- owl slider -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
     </script>
@@ -175,7 +175,7 @@
     <!-- SDK MercadoPago.js -->
     <script src="https://sdk.mercadopago.com/js/v2"></script>
     <!-- custom js -->
-    <script src="web/js/custom.js"></script>
+    <script src="/web/js/custom.js"></script>
     <!-- Google Map -->
     <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
     </script> -->
